d3164809f81eb341780db109fb71aba981766222
Gabriel Soares Maciel <gsm2015@outlook.com.br>
2023-03-08 21:07:55 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  swcMinify: true,
   experimental: {
     appDir: true,
+  },
+  webpack: (config, options) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
   },
 }
 
